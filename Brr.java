b34a875acb0494933b0cb9b419a56eac193ad2ce
Taian <taianmeca@gmail.com>
2017-07-19 21:31:46 -0300

    Add files via upload

---
+++ Brr.java
@@ -0,0 +1,9 @@
+
+public class Brr extends Money{
+	public Brr (int amount) {
+		this.amount=amount;
+	}
+	public Money times(int multiplier) {
+		return new Brr(amount*multiplier);
+	}
+}
---
+++ Dollar.java
@@ -0,0 +1,9 @@
+
+public class Dollar extends Money{
+	public Dollar (int amount) {
+		this.amount=amount;
+	}
+	public Money times(int multiplier) {
+		return new Dollar(amount*multiplier);
+	}
+}
